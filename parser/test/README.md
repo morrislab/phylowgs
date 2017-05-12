1b9c82f3cebc027624fc01744b7227a6e6b54897
Jeff Wintersinger <jeff.git@wintersinger.com>
2017-05-12 06:23:43 -0400

    Extend test suite to handle single & multi-sample CNAs.

---
+++ parser/test/README.md
@@ -0,0 +1,2 @@
+To execute the parser test suite, run `python2 run_tests.py`, with the working
+directory set to this directory.
--- parser/test/run_tests.py
+++ parser/test/run_tests.py
@@ -4,45 +4,97 @@ import filecmp
 import os
 import shutil
 
-def compare(vcf_format, input_vcf, good_ssm_output):
+#def compare(vcf_format, input_vcf, good_ssm_output):
+def compare_outputs(comparison, parser_params=None):
+  if parser_params is None:
+    parser_params = []
+  if 'cnv_path' not in comparison:
+    comparison['cnv_path'] = []
+
   output_dir = tempfile.mkdtemp()
   ssm_output_path = os.path.join(output_dir, 'output.ssm')
   cnv_output_path = os.path.join(output_dir, 'output.cnv')
-  cmd =[
+  cmd = [
     'python2',
     '../create_phylowgs_inputs.py',
-    '--regions', 'all',
     '--output-variants', ssm_output_path,
     '--output-cnvs', cnv_output_path,
-    '--vcf-type', 'S1=%s' % vcf_format,
-    'S1=%s' % input_vcf,
   ]
+  cmd += parser_params
+  for idx, vcf_format in enumerate(comparison['vcf_format']):
+    samp_name = 'S%s' % (idx + 1)
+    cmd += ['--vcf-type', '%s=%s' % (samp_name, vcf_format)]
+  for idx, cnv_path in enumerate(comparison['cnv_path']):
+    samp_name = 'S%s' % (idx + 1)
+    cmd += ['--cnvs', '%s=%s' % (samp_name, cnv_path)]
+  for idx, vcf_path in enumerate(comparison['vcf_path']):
+    samp_name = 'S%s' % (idx + 1)
+    cmd.append('%s=%s' % (samp_name, vcf_path))
+
   subprocess.call(cmd)
 
-  variant_output_matches = filecmp.cmp(ssm_output_path, good_ssm_output)
-  cnv_output_matches = os.path.getsize(cnv_output_path) == 0
-  shutil.rmtree(output_dir)
-  if variant_output_matches and cnv_output_matches:
-    print('%s passed (%s)' % (vcf_format, input_vcf))
+  files = (
+    (ssm_output_path, comparison['ssm_output_good']),
+    (cnv_output_path, comparison['cnv_output_good']),
+  )
+  all_match = True
+  for inf, outf in files:
+    all_match = all_match and filecmp.cmp(inf, outf)
+  #shutil.rmtree(output_dir)
+  if all_match:
+    print('%s passed' % comparison['name'])
   else:
-    raise Exception('Outputs do not match for %s' % vcf_format)
+    raise Exception('Outputs do not match for %s' % comparison['name'])
+
+def test_vcf_formats():
+  for vcf_format in ('vardict', 'strelka', 'mutect_tcga'):
+    comparison = {
+      'name':            'vcf_format_%s' % vcf_format,
+      'vcf_format':      [vcf_format],
+      'vcf_path':        [os.path.join('inputs',  vcf_format, vcf_format + '.vcf')],
+      'ssm_output_good': os.path.join('outputs', vcf_format, vcf_format + '.ssm'),
+      'cnv_output_good': os.path.join('outputs', vcf_format, vcf_format + '.cnv'),
+    }
+    compare_outputs(comparison, ['--regions', 'all'])
+
+def test_id_parsing():
+  vcf_format = 'mutect_tcga'
+  comparison = {
+    'name':            'id_parsing',
+    'vcf_format':      [vcf_format],
+    'vcf_path':        [os.path.join('inputs',  vcf_format, vcf_format + '_with_ids.vcf')],
+    'ssm_output_good': os.path.join('outputs', vcf_format, vcf_format + '_with_ids.ssm'),
+    'cnv_output_good': os.path.join('outputs', vcf_format, vcf_format + '_with_ids.cnv'),
+  }
+  compare_outputs(comparison, ['--regions', 'all'])
+
+def test_singlesamp_cnv():
+  comparison = {
+    'name':            'singlesamp_cnv',
+    'vcf_format':      ['pcawg_consensus'],
+    'vcf_path':        [os.path.join('inputs', 'singlesamp_cnv/S1.vcf.gz')],
+    'cnv_path':        [os.path.join('inputs', 'singlesamp_cnv/S1.cnv.txt')],
+    'ssm_output_good': os.path.join('outputs', 'singlesamp_cnv', 'ssm_data.txt'),
+    'cnv_output_good': os.path.join('outputs', 'singlesamp_cnv', 'cnv_data.txt'),
+  }
+  compare_outputs(comparison)
+
+def test_multisamp_cnvs():
+  comparison = {
+    'name':            'multisamp_cnvs',
+    'vcf_format':      2*['pcawg_consensus'],
+    'vcf_path':        [os.path.join('inputs', 'multisamp_cnvs/S%s.vcf.gz') % sidx for sidx in (1, 2)],
+    'cnv_path':        [os.path.join('inputs', 'multisamp_cnvs/S%s.cnv.txt') % sidx for sidx in (1, 2)],
+    'ssm_output_good': os.path.join('outputs', 'multisamp_cnvs', 'ssm_data.txt'),
+    'cnv_output_good': os.path.join('outputs', 'multisamp_cnvs', 'cnv_data.txt'),
+  }
+  compare_outputs(comparison)
 
 def main():
-  #for vcf_format in ('dkfz', 'muse', 'mutect_pcawg', 'mutect_smchet', 'sanger', 'vardict'):
-  for vcf_format in ('vardict', 'strelka', 'mutect_tcga'):
-    compare(
-      vcf_format,
-      os.path.join('inputs', vcf_format, vcf_format + '.vcf'),
-      os.path.join('outputs', vcf_format, vcf_format + '.ssm')
-    )
-  return
-
-  # test for ID parsing
-  vcf_format = 'mutect_tcga'
-  compare(
-    vcf_format,
-    os.path.join('inputs', vcf_format, vcf_format + '_with_ids.vcf'),
-    os.path.join('outputs', vcf_format, vcf_format + '_with_ids.ssm')
-  )
+  #test_vcf_formats()
+  test_singlesamp_cnv()
+  test_multisamp_cnvs()
+  # This test is broken right now. D'oh.
+  #test_id_parsing()
 
 main()
