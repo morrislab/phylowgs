64b7342a58b6d64e783a445f4d559303d11022ac
Jarry Barber <jarrybarber@gmail.com>
2018-05-01 15:56:45 -0400

    Reorganized multevolve

--- multevolve.py
+++ multevolve.py
@@ -6,11 +6,21 @@ import signal
 import zipfile
 import numpy as np
 
+def create_directory(dirname):
+    if not os.path.exists(dirname):
+        os.makedirs(dirname);  
+
+def logsumexp(a):
+    #numpy has a logaddexp() but it only takes two values and that's stupid so 
+    #I'm just going to create my own logsumexp here. 
+    max_a = np.max(a);
+    result = max_a + np.log(np.sum([np.exp(i-max_a) for i in a]))
+    return result
+
 def parse_args():
     parser = argparse.ArgumentParser(
       description='Concurrently run multiple chains of PhyloWGS.',
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter
-      )
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n', '--num-chains', dest='num_chains', default=10, type=int,
 		  help='Number of chains to run concurrently')
     parser.add_argument('-B', '--burnin-samples', dest='burnin_samples', default=1000, type=int,
@@ -20,7 +30,7 @@ def parse_args():
     parser.add_argument('-r', '--random-seeds', dest='random_seeds', default=[], type=list,
 		  help='Random seeds for initializing MCMC')
     parser.add_argument('-if', '--chain-inclusion-factor', dest='chain_inclusion_factor', default=1.5, type=float,
-		  help='Factor determining which chains will be included in the output "merged" folder. Default is 1.5, meaning that the sum of the likelihoods of the trees found in each chain much be greater than 1.5x the maximum of that value across chains. Setting this value = inf includes all chains and setting it = 1 will include only the best chain.')
+		  help='Factor for determining which chains will be included in the output "merged" folder. Default is 1.5, meaning that the sum of the likelihoods of the trees found in each chain much be greater than 1.5x the maximum of that value across chains. Setting this value = inf includes all chains and setting it = 1 will include only the best chain.')
     parser.add_argument('-od', '--output-directory', dest='output_directory', default='', type=str,
 		  help='Directory where results from each chain will be saved. If directory does not exist, will attempt to create it here. (Default = "current_directory/multevolve_chains")')
     parser.add_argument('-sf','--ssm-file',dest='ssm_file',
@@ -29,7 +39,6 @@ def parse_args():
 		help='File listing CNVs (copy number variations).')
     args, evolve_args = parser.parse_known_args()
     args = dict(args._get_kwargs());
-
     return args, evolve_args
 
 def check_args(args):
@@ -45,12 +54,7 @@ def check_args(args):
     #list of random seeds, if this was provided by the user, has length = num_chains.
     if len(args['random_seeds']) != args['num_chains']:
         raise ValueError("Number of chains is not equal to the number of input seeds.");
-    
-    return args;
-
-def create_directory(dirname):
-    if not os.path.exists(dirname):
-        os.makedirs(dirname);   
+    return args
 
 def run_chains(args,evolve_args):
     '''
@@ -85,13 +89,13 @@ def run(args,evolve_args,chain_index,working_dir,current_dir,output_dir):
         os.path.join(current_dir,args['cnv_file'])];
     bashCommand = bashCommand + list(evolve_args);
     print "Starting chain " + str(chain_index);
-    process = subprocess.Popen(bashCommand,stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=output_dir, universal_newlines=True,bufsize=1)
+    process = subprocess.Popen(bashCommand,stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=output_dir, universal_newlines=True, bufsize=1)
     return process
 
 def watch_chains(args,processes):
     '''
     This watches the subprocesses as they run. Will check to see if they
-    are complete and if so, move on to the next step; and if not, then will
+    are complete and if so move on to the next step, and if not then will
     capture their outputs and inform the user of each processes progression.
     '''
     def read_stdout_alarm_handler(signum, frame):
@@ -109,7 +113,7 @@ def watch_chains(args,processes):
         # Capture the output from each process, modify it, and output it. 
         # Note: process.stdout returns a file handle from which you can read outputs, however if you
         # call readline and there is no new information to read, it will wait until there is until
-        # returning, which is really annoying. So I set up a timer here to throw an exception if
+        # returning which is really annoying. So I set up a timer here to throw an exception if
         # more than 0.1 seconds passes so that we can move on to read info from another process that may already 
         # have output to read.
         other_text = [];
@@ -133,17 +137,8 @@ def watch_chains(args,processes):
               other_text.append("chain{}: {}".format(chain_index,new_line))
         print "\033[F"*(num_chains+1), # Move cursor up to line that starts telling us about chain progression. Want to overwrite those lines.
         print "".join(other_text),
-        print " "*70
+        print " "*70 #blank line between "other text" and progression text.
         print "".join(progression_text),
-
-def logsumexp(a):
-    '''
-    numpy as a logaddexp() but it only takes two values and that's stupid so 
-    I'm just going to create my own logsumexp here.
-    '''
-    max_a = np.max(a);
-    result = max_a + np.log(np.sum([np.exp(i-max_a) for i in a]))
-    return result
 
 def determine_chains_to_merge(chain_dirs,chain_inclusion_factor):
     '''
@@ -153,17 +148,15 @@ def determine_chains_to_merge(chain_dirs,chain_inclusion_factor):
     '''
     logSumLHs = []
     for chain_dir in chain_dirs:
-        logLHs = [];
+        logLHs = []
         tree_zip_file = zipfile.ZipFile(os.path.join(chain_dir,'trees.zip'), mode = 'r')
         for tree_name in tree_zip_file.namelist():
             if "tree" in tree_name:
-                #likelihood is in the names of the trees, just use that.
+                #the logged likelihood is in the names of the trees, just use that.
                 logLHs.append(float(tree_name.split('_')[-1]))
         logSumLHs.append(logsumexp(logLHs))
-    print logSumLHs
-    best_chain = np.argmax(logSumLHs);
-    bestLogSumLH = np.max(logSumLHs);
-    chains_to_merge = [i for i,logSumLH in enumerate(logSumLHs) if logSumLH > (bestLogSumLH*chain_inclusion_factor)]
+    bestLogSumLH = np.max(logSumLHs)
+    chains_to_merge = [i for i,logSumLH in enumerate(logSumLHs) if logSumLH > (chain_inclusion_factor*bestLogSumLH)]
     return chains_to_merge
 
 def merge_best_chains(args,chain_dirs,chains_to_merge):
@@ -204,9 +197,6 @@ def merge_best_chains(args,chain_dirs,chains_to_merge):
     print "To delete individual chain information, just type 'rm /path/to/output/dir/multevolve_chains/chain_*/trees.zip'"
 
 def main():
-    #To do:
-    # - fix up helper text that goes with "inclusion_factor" in parseargs.
-    # - write a class for the multevolve chain output handler? Can input all of the processes, keep track of progression text, read from stdouts, contain the signal.alarm handler
     args,evolve_args = parse_args();
     check_args(args);
     chain_dirs = run_chains(args,evolve_args);
