04e02cefb3b782faac819040ca90e4538f24c7c9
AL Brown <brown.annaleigh@gmail.com>
2019-01-06 18:16:09 -0500

    Changing pathing of index_data.py
    Script can now be called from a different folder, allowing easier integration into a pipeline

--- witness/index_data.py
+++ witness/index_data.py
@@ -7,22 +7,25 @@ from collections import defaultdict
 
 def main():
   datasets = defaultdict(list)
+  #use a path from the location of the python script so that it can be called from outside the same folder
+  base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data')
 
-  base_dir = 'data'
   for run_name in os.listdir(base_dir):
+
     for summary_path in glob.glob(os.path.join(base_dir, run_name, '*.summ.json')):
       dataset_name = summary_path.split('/')[-1].split('.')[0]
+
       run = {
-        'summary_path': summary_path,
-        'muts_path': os.path.join(base_dir, run_name, dataset_name + '.muts.json'),
+        'summary_path': os.path.join('data', run_name, dataset_name + '.summ.json'),
+        'muts_path': os.path.join('data', run_name, dataset_name + '.muts.json'),
         'name': dataset_name,
       }
 
-      clusters_path = os.path.join(base_dir, run_name, dataset_name + '.clusters.json')
+      clusters_path = os.path.join('data', run_name, dataset_name + '.clusters.json')
       if os.path.isfile(clusters_path):
         run['clusters_path'] = clusters_path
 
-      mutass_path = os.path.join(base_dir, run_name, dataset_name + '.mutass')
+      mutass_path = os.path.join('base_dir', run_name, dataset_name + '.mutass')
       if os.path.isdir(mutass_path):
         run['mutass_path'] = mutass_path
 
